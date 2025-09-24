b03970e627289b6a77460eec332edf6e9e92cf2d
nlmongwe <nlmongwe@gmail.com>
2025-09-24 07:37:28 +0200

    first commit

---
+++ lib/UI/find_a_grave/views/find_a_grave_mobile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FindAGraveMobileView extends StatelessWidget {
+  const FindAGraveMobileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Find a grave mobile"),
+    );
+  }
+}
